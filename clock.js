0c151d1b0951aecf75dbea39ee6df695ee3b6637
callumjhb <callumjhbenfell@gmail.com>
2024-03-23 13:42:05 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -11,18 +11,30 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  
+  noStroke();
+  background(50); 
+  
+  //
+  fill(27, 32, 36);
+  triangle(0, 0, 290.6, 0, 0, 51);
 
+  //Foreground bar structure
 
-  fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+  fill(47, 62, 65);
+  quad(404.2, 0, 404.2, 424.6, 960, 522.6, 960, 0); // building 
+  
+  fill(63, 91, 83);
+  quad(404.2, 159.5, 960, 257, 960, 289, 393.7, 189); // rim
+  quad(408.2, 345.2, 960, 442.6, 960, 446.4, 404.2, 348.4); // window light
+  
+  fill(242, 231, 139);
+  quad(408.2, 201, 960, 299, 960, 442.6, 408.2, 345); //window
+  
+  fill(47, 62, 65);
+  quad(495.5, 216, 499, 217, 499.5, 361.4, 495.5, 360.7,); // window frame left
+  quad(844, 278, 847.5, 279.3, 848.3, 423.6, 844, 422.2); // window frame right
+  quad(495.5, 360.6, 499.6, 361.4, 495.6, 364.6, 491.5, 363.8); // window frame left shadow
+  quad(844.5, 422, 848.6, 422.8, 844.6, 426, 840.5, 425.3); // window frame right shadow
 
 }
